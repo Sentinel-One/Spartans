21ad3b8cec195d8804dbfae854faa530969b6859
deanla@gmail.com <deanla@gmail.com>
2020-03-29 13:00:28 +0300

    Bump version: 0.1.0 → 0.2.0

--- spartans/__init__.py
+++ spartans/__init__.py
@@ -4,7 +4,7 @@ from .feature_matrix import FeatureMatrix
 
 __author__ = """Dean Langsam"""
 __email__ = 'deanla@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 """SPARTANS - SPARse Tools for ANalysiS"""
 
